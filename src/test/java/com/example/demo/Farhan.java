1d83110b1b84f5d175e6569a96b2925c7b223f2a
rehansharaf <rehansharaf2011@gmail.com>
2023-04-25 15:33:07 +0500

    Update Farhan.java

--- src/test/java/com/example/demo/Farhan.java
+++ src/test/java/com/example/demo/Farhan.java
@@ -7,6 +7,7 @@ public class Farhan {
     System.out.println("Method created by Farhan");
     System.out.println("Method created by Farhan2");
    System.out.println("New Method");
+   System.out.println("New Method");
    
   }
   
